bb00329250446372d8abf655b0a89fe5d3bfff87
SolLysLe <158502841+SolLysLe@users.noreply.github.com>
2026-05-07 20:51:06 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, FallingEdge, Timer
 from cocotb.utils import get_sim_time
 import math
 
@@ -25,34 +25,47 @@ async def simulate_spi_frame(dut, data):
     Simulate a 24 bit SPI transmission
     """
     SCK = dut.SCK
-    SDI = dut.ui_in[1]
+    # Sử dụng thao tác bit an toàn cho ui_in
+    def set_sdi(val):
+        current_ui = int(dut.ui_in.value)
+        if val:
+            dut.ui_in.value = current_ui | (1 << 1)
+        else:
+            dut.ui_in.value = current_ui & ~(1 << 1)
 
-    SDI.value = data[0]
+    set_sdi(data[0])
 
-    sck = Clock(SCK, SPI_PERIOD, units="us")
+    sck = Clock(SCK, SPI_PERIOD, unit="us")
     sck_gen = cocotb.start_soon(sck.start())
     for i in range(1, 24):
         await FallingEdge(SCK)
-        SDI.value = data[i]
+        set_sdi(data[i])
 
     await FallingEdge(SCK)
 
     sck_gen.kill()
     SCK.value = 0
 
-    await Timer(SPI_PERIOD / 2, units="us")
+    await Timer(SPI_PERIOD / 2, unit="us")
 
 
 async def simulate_frame_input(dut, data):
     """
     Given a pixel's worth of data, simulate a frame transmission (64 sequential SPI transmissions)
     """
-    CS = dut.ui_in[2]
+    # Hàm hỗ trợ set CS (ui_in[2])
+    def set_cs(val):
+        current_ui = int(dut.ui_in.value)
+        if val:
+            dut.ui_in.value = current_ui | (1 << 2)
+        else:
+            dut.ui_in.value = current_ui & ~(1 << 2)
+
     for i in range(0, 64):
-        dut.value = 0
+        set_cs(0) # Sửa lỗi dut.value = 0 thành hạ CS xuống 0
         await simulate_spi_frame(dut, data)
-        dut..value = 1
-        await Timer(SPI_PERIOD, units="us")
+        set_cs(1) # Sửa lỗi dut..value = 1 thành nâng CS lên 1
+        await Timer(SPI_PERIOD, unit="us")
 
 
 @cocotb.test()
@@ -60,7 +73,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 10, unit="us")
     clock_gen = cocotb.start_soon(clock.start())
 
     # Reset
@@ -76,29 +89,25 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    CLK = dut.clk
-    CS.value = 1
-    ui_in_val = dut.ui_in.value
-    CS = ui_in_val[2]
-    CS.value = 1
+    # Khởi tạo CS ban đầu (ui_in[2] = 1)
+    dut.ui_in.value = int(dut.ui_in.value) | (1 << 2)
 
-    clock = Clock(CLK, CLOCK_PERIOD, units="us")
+    clock = Clock(dut.clk, CLOCK_PERIOD, unit="us")
     cocotb.start_soon(clock.start())
 
-    await Timer(500, units="us")
+    await Timer(500, unit="us")
     await reset(dut)
-    await Timer(500, units="us")
+    await Timer(500, unit="us")
 
     data = [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
 
     await simulate_frame_input(dut, data)
 
-    await Timer(500, units="us")
+    await Timer(500, unit="us")
 
     data = [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]
     await simulate_frame_input(dut, data)
 
-    await Timer(500, units="us")
+    await Timer(500, unit="us")
 
     await ClockCycles(dut.clk, 1000)
